0f07473526cfcd289c7624690fca2fa6f59120c4
vedashrith <vedashrithgoud@gmail.com>
2021-02-21 17:45:56 +0530

    asgn-1

---
+++ DigitFrequency.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char str[1000];
+    int num,i,j,cnt,ch=48;
+    
+    scanf("%[^\n]%*c",&str);
+    num=strlen(str);
+    for(i=0;i<10;i++)
+    {
+        cnt=0;
+        for(j=0;j<num;j++)
+        {
+            if(str[j]==ch)
+            {
+                cnt++;
+            }
+        }
+        printf("%d ",cnt);
+            ch++;
+    }
+    
+    
+    return 0;
+}
+
